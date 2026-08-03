65d9b657756289b21335cf79453446a0eca37970
brawlitup99 <63270996+brawlitup99@users.noreply.github.com>
2026-08-02 21:54:35 -0700

    8-2-26 Blogpost

--- docs/script.js
+++ docs/script.js
@@ -34,7 +34,10 @@ let postsArray = [
 //[ "posts/2020-11-10-Special-Characters-Example.html", encodeURI( 'Spéci@l "Character\'s" Examp|e' ) ],
 //[ "posts/2020-11-10-My-Third-Post-Example.html" ],
 //[ "posts/2020-11-10-My-Second-Post-Example.html" ],
-[ "posts/2026-02-10-New-Site-LAUNCHED!.html" ] ];
+[ "posts/2026-02-10-New-Site-LAUNCHED!.html" ], 
+[ "posts/2026-08-02-Game-Jam-Game-OUT!!-+-Looking-For-Work.html" ]
+
+];
 
 //XXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 
